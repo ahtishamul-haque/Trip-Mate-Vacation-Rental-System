f1d247f0571244e10eacdaaf84a59653dcc3b2e8
Ahtishamul Haque <112795299+ahtishamul-haque@users.noreply.github.com>
2025-06-18 17:41:24 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,7 +2,7 @@ const Listing = require("../models/listing.js");
 
 module.exports.index = async(req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index", {allListings});
+    res.render("listings/index.ejs", {allListings});
 };
 
 module.exports.renderNewForm = (req, res) => {
